34b7914a73e2fc228b3942c589c427829bfa9c00
iamukn <n.u.kingsley@gmail.com>
2023-06-27 22:09:12 +0000

    block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,8 +4,8 @@ export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
 
-    var task = true;
-    var task2 = false;
+     task = true;
+     task2 = false;
   }
 
   return [task, task2];

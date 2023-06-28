12f0d588e97f8cdcc9f571cf5cecf2e4c6a2bbea
iamukn <n.u.kingsley@gmail.com>
2023-06-28 22:43:06 +0000

    promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({firstName, lastName,}); 
+  return new Promise((resolve) => {
+    resolve({
+      firstName,
+      lastName,
+    });
+  });
 }

344ec169afd6892eee4c72887c6570c396aeef0b
iamukn <n.u.kingsley@gmail.com>
2023-06-28 17:20:06 +0000

    hot fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
 export default function getResponseFromAPI() {
-  const data = new Promise((res, rej) => {
-    setTimeout(() => {
-      res(2 * 2);
-    }, 5000);
-  });
-  return data.then((res) => console.log(res)).catch((err) => console.err(err));
+  return new Promise((res, rej) => {};
 }

38eda3504701b6e6229e4c13c3ba9b54f73f79c3
iamukn <n.u.kingsley@gmail.com>
2023-06-28 22:17:10 +0000

    hot fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
-  const image = uploadPhoto();
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
   const user = createUser();
 
-  return Promise.all([image, user]).then((res) => {
+  return Promise.all([photo, user]).then((res) => {
     console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
   })
     .catch(() => { console.log('Signup system offline'); });

b0b41047a78e1eae3830f6d53b47c67f5dfa31f5
iamukn <n.u.kingsley@gmail.com>
2023-06-28 22:05:26 +0000

    hot fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,5 +7,5 @@ export default function handleProfileSignup () {
   return Promise.all([d1, d2]).then((res) => {
     console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
   })
-    .catch((err) => { console.log('Signup system offline'); });
+    .catch(() => { console.log('Signup system offline'); });
 }

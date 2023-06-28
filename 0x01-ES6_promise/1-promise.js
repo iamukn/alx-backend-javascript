4348ff7bd61dbd25f5ab23b50dcbdde699316854
iamukn <n.u.kingsley@gmail.com>
2023-06-28 20:25:24 +0000

    hot fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,16 @@
 export default function getFullResponseFromAPI(success) {
-    const promise = new Promise ((resolve, reject) => {
-
-        if (success === true) {
-            resolve({
-                ["status"]: 200,
-                ["body"]: "Success",
-            });
-        } else {
-            reject("Error: The fake API is not working currently");
-        }
-    });
-
-    return promise;
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve( {
+            ["status"]: 200,
+            ["body"]: "Success",
+        });
+    }
+    else {
+        reject(new Error('The fake API is not working currently'));
+    }
+});
 }
+
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));

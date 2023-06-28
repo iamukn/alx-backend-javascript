c62e0a1e3010a41840ab547b71c2e76b0906fd08
iamukn <n.u.kingsley@gmail.com>
2023-06-28 20:57:14 +0000

    hot fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if(promise === Promise.resolve()) {
-      resolve({status:200, body: success});
-  } else {
-      reject(new Error(""));
-  }
-}).then((res) => {console.log("Got a response from the API");}, (err) => {console.log("Got a response from the API");});
+  return promise
+    .then(() => {status: 200, body: 'success'})
+    .catch(() => Error())
+    .finally(() => {console.log('Got a response from the API'); });
+
 }

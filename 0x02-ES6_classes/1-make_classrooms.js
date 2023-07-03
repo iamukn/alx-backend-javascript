e3b3321f6d8d766f5a808bd2fc9473c0af683e05
iamukn <n.u.kingsley@gmail.com>
2023-07-03 16:42:20 +0000

    more classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms() {
+export default function initializeRooms () {
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);

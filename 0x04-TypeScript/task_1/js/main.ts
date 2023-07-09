75c24b317cbd6b2811e0c2901e201838acea6d33
iamukn <n.u.kingsley@gmail.com>
2023-07-09 20:21:40 +0000

    erased unnecessary object instance

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,14 +6,3 @@ interface Teacher {
     location: string;
     [index: string]: any;
 }
-
-
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);

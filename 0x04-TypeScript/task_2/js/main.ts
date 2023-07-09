eaca751e987d1764ffa93dc0019fd6e80bca504e
iamukn <n.u.kingsley@gmail.com>
2023-07-09 22:02:08 +0000

    returning objects from a method

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,15 +1,51 @@
-interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [index: string]: any;
+// Director interface with methods that return a string
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
 }
 
-
-interface Directors extends Teacher {
-    numberOfReports: number;
+// TeacherInterface with methods returning strings
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
 }
 
+// Director class that implements DirectorInterface
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
 
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+// Teacher class that implements TeacherInterface
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: number | string): Teacher | Director {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher;
+    }
+    return new Director;
+}
+console.log(createEmployee(500))

6ddb97762758f09bca209e96ddb8fc00589943a5
iamukn <n.u.kingsley@gmail.com>
2023-07-09 20:28:09 +0000

    extends with interface

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,15 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [index: string]: any;
+}
+
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+
